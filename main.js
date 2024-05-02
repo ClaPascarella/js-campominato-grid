6abbd6ac4c2d6984d34fda214bca2670cfbbe346
ClaPascarella <claudiopascarella1996@gmail.com>
2024-05-02 13:13:40 +0200

    file css html e js

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 let griglia = document.getElementById("grid");
+let start = document.getElementById("start")
+let select = document.getElementById("difficulty")
+
 
 for ( let i = 1; i <= 100; i++) {
     let elemento =creaQuadrato(i);
@@ -6,17 +9,53 @@ for ( let i = 1; i <= 100; i++) {
 
 }
 
-function creaQuadrato(contenuto) {
+start.addEventListener("click", function (){
+	griglia.innerHTML = "";
 
+	griglia.classList.remove("gridmedium","gridhard","grideasy")
+	
+	let gridSize;
+	let gridDifficulty;
+
+	if (select.value =="easy") {
+	   gridSize = 100;
+	   gridDifficulty ="grideasy"
+		
+	}else if (select.value =="medium") {
+		gridSize= 81;
+		gridDifficulty ="gridmedium"
+
+	}else if (select.value =="hard") {
+		gridSize= 49;
+		gridDifficulty ="gridhard"
+
+	}
+	griglia.classList.add(gridDifficulty)
+	for (let i= 1; i<=gridSize; i++) {
+		let quadrato = mySquare(i)
+		griglia.append(quadrato);
+	}
+})
+
+function mySquare (contenuto) {
 	let square = document.createElement("div");
 	square.classList.add("square");
-    square.innerText = contenuto;
 
-	
 	square.addEventListener("click", function() {
-	console.log("cliccato", this); 
-	this.classList.toggle("evidenzia");
-	});
+
+		if (square.classList.includes("evidenzia")) {
+			square.innerHTML ="";
+		}else {
+			square.innerText = contenuto;
+		}
+
+		square.classList.toggle("evidenzia")
+	})
 
 	return square;
 }
+
+
+
+
+	
